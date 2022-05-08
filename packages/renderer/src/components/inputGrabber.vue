be43aeef104d6a0e09c147e2fa18ba0f06b80739
Kal'tist <kerushii@protonmail.com>
2022-05-07 22:57:49 -0400

    NO

--- packages/renderer/src/components/inputGrabber.vue
+++ packages/renderer/src/components/inputGrabber.vue
@@ -22,7 +22,15 @@ export default {
 }
 </script>
 <template>
-  wf
+  <div style="position:absolute;top:0;left:0;width:100%;height:100%;background:black;">
+    <div style="position:absolute;top:0;left:0;width:100%;height:100%;background:black;">
+      <div style="position: absolute; top: 32%; left: 0px; width: 100%; height: 28%; background: linear-gradient(18deg, #3a4085, #4685c5);">
+
+      </div>
+      <div style="position: absolute; top: 32%; left: 0px; width: 100%; height: 28%; background: repeating-linear-gradient(56deg,#fff0,#ffffff30 3px,#46529800 7px,#46529800 17px);">
+      </div>
+    </div>
+  </div>
 </template>
-<style scoped>
+  <style scoped>
 </style>
