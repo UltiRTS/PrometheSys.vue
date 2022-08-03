f3d135e8348c1ca9a4e1a1748dbc78f34a4ac048
bianyuanop <m13320507359@163.com>
2022-08-03 15:16:20 +0800

    typed login

--- packages/renderer/src/pages/login.vue
+++ packages/renderer/src/pages/login.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { numberLiteralTypeAnnotation } from '@babel/types'
 import { computed } from '@vue/reactivity'
 import { onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue'
 
@@ -70,10 +71,10 @@ onMounted(() => {
   let cosY = 0
   let cosX = cosY
 
-  const connections = []
-  const toDevelop = []
+  const connections: Connection[] = []
+  const toDevelop: Connection[] = []
   const data = []
-  const all = []
+  const all: (Connection | Data)[] = []
   let tick = 0
   const totalProb = 0
 
@@ -96,7 +97,7 @@ onMounted(() => {
     toDevelop.length = 0
 
     const connection = new Connection(0, 0, 0, opts.baseSize)
-    connection.step = Connection.rootStep
+    connection.step = connection.rootStep
     connections.push(connection)
     all.push(connection)
     connection.link()
@@ -112,171 +113,240 @@ onMounted(() => {
     }
   }
 
-  function Connection(x: number, y: number, z: number, size: number) {
-    this.x = x
-    this.y = y
-    this.z = z
-    this.size = size
-
-    this.screen = {}
-
-    this.links = []
-    this.probabilities = []
-    this.isEnd = false
-
-    this.glowSpeed = opts.baseGlowSpeed + opts.addedGlowSpeed * Math.random()
-  }
-
-  Connection.prototype.link = function () {
-    if (this.size < opts.minSize)
-      return this.isEnd = true
-
-    const links = []
-    const connectionsNum = opts.baseConnections + Math.random() * opts.addedConnections | 0
-    let attempt = opts.connectionAttempts
-
-    let alpha; let beta; let len
-    let cosA; let sinA; let cosB; let sinB
-    const pos: { x: number; y: number; z: number } = {
-      x: 0,
-      y: 0,
-      z: 0,
+  class Connection {
+    x: number
+    y: number
+    z: number
+    size: number
+    links: Connection[]
+    probabilities: []
+    isEnd: boolean
+    glowSpeed: number
+    screen: {
+      x: number
+      y: number
+      z: number
+      scale: number
+      color: string
+      lastX: number
+      lastY: number
     }
 
-    let passedExisting
-    let passedBuffered
+    step: () => void
+    rootStep = () => {
+      this.setScreen()
+      this.screen.color = opts.rootColor
+        .replace('light', `${30 + ((tick * this.glowSpeed) % 30)}`)
+        .replace('alp', `${(1 - this.screen.z / mostDistant) * 0.8}`)
 
-    while (links.length < connectionsNum && --attempt > 0) {
-      alpha = Math.random() * Math.PI
-      beta = Math.random() * Tau
-      len = opts.baseDist + opts.addedDist * Math.random()
+      for (let i = 0; i < this.links.length; ++i) {
+        ctx.moveTo(this.screen.x, this.screen.y)
+        ctx.lineTo(this.links[i].screen.x, this.links[i].screen.y)
+      }
+    }
 
-      cosA = Math.cos(alpha)
-      sinA = Math.sin(alpha)
-      cosB = Math.cos(beta)
-      sinB = Math.sin(beta)
+    constructor(x: number, y: number, z: number, size: number) {
+      this.x = x
+      this.y = y
+      this.z = z
+      this.size = size
 
-      pos.x = this.x + len * cosA * sinB
-      pos.y = this.y + len * sinA * sinB
-      pos.z = this.z + len * cosB
+      this.screen = {
+        x: 0,
+        y: 0,
+        z: 0,
+        scale: 0,
+        color: '#fff',
+        lastX: 0,
+        lastY: 0,
+      }
 
-      if (pos.x * pos.x + pos.y * pos.y + pos.z * pos.z < squareRange) {
-        passedExisting = true
-        passedBuffered = true
-        for (var i = 0; i < connections.length; ++i) {
-          if (squareDist(pos, connections[i]) < squareAllowed)
-            passedExisting = false
+      this.links = []
+      this.probabilities = []
+      this.isEnd = false
+
+      this.glowSpeed = opts.baseGlowSpeed + opts.addedGlowSpeed * Math.random()
+
+      this.step = () => {
+        this.setScreen()
+        this.screen.color = (this.isEnd ? opts.endColor : opts.connectionColor)
+          .replace('light', `${300 + ((tick * this.glowSpeed) % 300)}`)
+          .replace('alp', `${4 + (1 - this.screen.z / mostDistant) * 0.8}`)
+
+        for (let i = 0; i < this.links.length; ++i) {
+          ctx.moveTo(this.screen.x, this.screen.y)
+          ctx.lineTo(this.links[i].screen.x, this.links[i].screen.y)
         }
+      }
+    }
 
-        if (passedExisting) {
-          for (var i = 0; i < links.length; ++i) {
-            if (squareDist(pos, links[i]) < squareAllowed)
-              passedBuffered = false
+    link() {
+      if (this.size < opts.minSize)
+        return this.isEnd = true
+
+      const links = []
+      const connectionsNum = opts.baseConnections + Math.random() * opts.addedConnections | 0
+      let attempt = opts.connectionAttempts
+
+      let alpha; let beta; let len
+      let cosA; let sinA; let cosB; let sinB
+      const pos: { x: number; y: number; z: number } = {
+        x: 0,
+        y: 0,
+        z: 0,
+      }
+
+      let passedExisting
+      let passedBuffered
+
+      while (links.length < connectionsNum && --attempt > 0) {
+        alpha = Math.random() * Math.PI
+        beta = Math.random() * Tau
+        len = opts.baseDist + opts.addedDist * Math.random()
+
+        cosA = Math.cos(alpha)
+        sinA = Math.sin(alpha)
+        cosB = Math.cos(beta)
+        sinB = Math.sin(beta)
+
+        pos.x = this.x + len * cosA * sinB
+        pos.y = this.y + len * sinA * sinB
+        pos.z = this.z + len * cosB
+
+        if (pos.x * pos.x + pos.y * pos.y + pos.z * pos.z < squareRange) {
+          passedExisting = true
+          passedBuffered = true
+          for (let i = 0; i < connections.length; ++i) {
+            if (squareDist(pos, connections[i]) < squareAllowed)
+              passedExisting = false
+          }
+
+          if (passedExisting) {
+            for (let i = 0; i < links.length; ++i) {
+              if (squareDist(pos, links[i]) < squareAllowed)
+                passedBuffered = false
+            }
+          }
+
+          if (passedExisting && passedBuffered) {
+            links.push({
+              x: pos.x,
+              y: pos.y,
+              z: pos.z,
+            })
           }
         }
+      }
 
-        if (passedExisting && passedBuffered) {
-          links.push({
-            x: pos.x,
-            y: pos.y,
-            z: pos.z,
-          })
+      if (links.length === 0) { this.isEnd = true }
+      else {
+        for (let i = 0; i < links.length; ++i) {
+          const pos = links[i]
+          const connection
+            = new Connection(pos.x, pos.y, pos.z, this.size * opts.sizeMultiplier)
+
+          this.links[i] = connection
+          all.push(connection)
+          connections.push(connection)
         }
+        for (let i = 0; i < this.links.length; ++i)
+          toDevelop.push(this.links[i])
       }
     }
 
-    if (links.length === 0) { this.isEnd = true }
-    else {
-      for (var i = 0; i < links.length; ++i) {
-        const pos = links[i]
-        const connection
-          = new Connection(pos.x, pos.y, pos.z, this.size * opts.sizeMultiplier)
+    draw() {
+      ctx.fillStyle = this.screen.color
+      ctx.beginPath()
+      if (this.screen.x < 0 || this.screen.y < 0 || this.screen.scale < 0 || this.size < 0 || Tau < 0)
+        return
 
-        this.links[i] = connection
-        all.push(connection)
-        connections.push(connection)
+      ctx.arc(this.screen.x, this.screen.y, this.screen.scale * this.size, 0, Tau)
+      ctx.fill()
+    }
+
+    setScreen() {
+      let x = this.x
+      let y = this.y
+      let z = this.z
+
+      // apply rotation on X axis
+      const Y = y
+      y = y * cosX - z * sinX
+      z = z * cosX + Y * sinX
+
+      // rot on Y
+      const Z = z
+      z = z * cosY - x * sinY
+      x = x * cosY + Z * sinY
+
+      this.screen.z = z
+
+      // translate on Z
+      z += opts.depth
+
+      this.screen.scale = opts.focalLength / z
+      this.screen.x = opts.vanishPoint.x + x * this.screen.scale
+      this.screen.y = opts.vanishPoint.y + y * this.screen.scale
+    }
+  }
+
+  class Data {
+    glowSpeed: number
+    connection: Connection
+    nextConnection: Connection
+    x: number
+    y: number
+    z: number
+    size: number
+    ox: number
+    oy: number
+    oz: number
+    os: number
+    nx: number
+    ny: number
+    nz: number
+    ns: number
+    dx: number
+    dy: number
+    dz: number
+    ds: number
+    speed: number
+    screen: {
+      x: number
+      y: number
+      z: number
+      scale: number
+      color: string
+      lastY: number
+      lastX: number
+    }
+
+    proportion: number
+    ended: number
+
+    constructor(connection: Connection) {
+      this.x = 0
+      this.y = 0
+      this.z = 0
+      this.size = 0
+      this.ended = 2
+      this.glowSpeed = opts.baseGlowSpeed + opts.addedGlowSpeed * Math.random()
+      this.speed = opts.baseSpeed + opts.addedSpeed * Math.random()
+
+      this.screen = {
+        x: 0,
+        y: 0,
+        z: 0,
+        scale: 0,
+        color: '',
+        lastY: 0,
+        lastX: 0,
       }
-      for (var i = 0; i < this.links.length; ++i)
-        toDevelop.push(this.links[i])
-    }
-  }
-  Connection.prototype.step = function () {
-    this.setScreen()
-    this.screen.color = (this.isEnd ? opts.endColor : opts.connectionColor)
-      .replace('light', `${300 + ((tick * this.glowSpeed) % 300)}`)
-      .replace('alp', `${4 + (1 - this.screen.z / mostDistant) * 0.8}`)
 
-    for (let i = 0; i < this.links.length; ++i) {
-      ctx.moveTo(this.screen.x, this.screen.y)
-      ctx.lineTo(this.links[i].screen.x, this.links[i].screen.y)
-    }
-  }
-  Connection.rootStep = function () {
-    this.setScreen()
-    this.screen.color = opts.rootColor
-      .replace('light', `${30 + ((tick * this.glowSpeed) % 30)}`)
-      .replace('alp', `${(1 - this.screen.z / mostDistant) * 0.8}`)
+      if (connection.isEnd)
+        connection = connections[0]
 
-    for (let i = 0; i < this.links.length; ++i) {
-      ctx.moveTo(this.screen.x, this.screen.y)
-      ctx.lineTo(this.links[i].screen.x, this.links[i].screen.y)
-    }
-  }
-  Connection.prototype.draw = function () {
-    ctx.fillStyle = this.screen.color
-    ctx.beginPath()
-    if (this.screen.x < 0 || this.screen.y < 0 || this.screen.scale < 0 || this.size < 0 || Tau < 0)
-      return
-
-    ctx.arc(this.screen.x, this.screen.y, this.screen.scale * this.size, 0, Tau)
-    ctx.fill()
-  }
-
-  function Data(connection) {
-    this.glowSpeed = opts.baseGlowSpeed + opts.addedGlowSpeed * Math.random()
-    this.speed = opts.baseSpeed + opts.addedSpeed * Math.random()
-
-    this.screen = {}
-
-    this.setConnection(connection)
-  }
-  Data.prototype.reset = function () {
-    this.setConnection(connections[0])
-    this.ended = 2
-  }
-  Data.prototype.step = function () {
-    this.proportion += this.speed
-
-    if (this.proportion < 1) {
-      this.x = this.ox + this.dx * this.proportion
-      this.y = this.oy + this.dy * this.proportion
-      this.z = this.oz + this.dz * this.proportion
-      this.size = (this.os + this.ds * this.proportion) * opts.dataToConnectionSize
-    }
-    else { this.setConnection(this.nextConnection) }
-
-    this.screen.lastX = this.screen.x
-    this.screen.lastY = this.screen.y
-    this.setScreen()
-    this.screen.color = opts.dataColor
-      .replace('light', `${400 + ((tick * this.glowSpeed) % 500)}`)
-      .replace('alp', `${0.2 + (1 - this.screen.z / mostDistant) * 0.6}`)
-  }
-  Data.prototype.draw = function () {
-    if (this.ended)
-      return --this.ended // not sre why the thing lasts 2 frames, but it does
-
-    ctx.beginPath()
-    ctx.strokeStyle = this.screen.color
-    ctx.lineWidth = this.size * this.screen.scale
-    ctx.moveTo(this.screen.lastX, this.screen.lastY)
-    ctx.lineTo(this.screen.x, this.screen.y)
-    ctx.stroke()
-  }
-  Data.prototype.setConnection = function (connection) {
-    if (connection.isEnd) { this.reset() }
-
-    else {
+      // should be setConnection(connection)
       this.connection = connection
       this.nextConnection = connection.links[connection.links.length * Math.random() | 0]
 
@@ -297,33 +367,96 @@ onMounted(() => {
 
       this.proportion = 0
     }
+
+    setScreen() {
+      let x = this.x
+      let y = this.y
+      let z = this.z
+
+      // apply rotation on X axis
+      const Y = y
+      y = y * cosX - z * sinX
+      z = z * cosX + Y * sinX
+
+      // rot on Y
+      const Z = z
+      z = z * cosY - x * sinY
+      x = x * cosY + Z * sinY
+
+      this.screen.z = z
+
+      // translate on Z
+      z += opts.depth
+
+      this.screen.scale = opts.focalLength / z
+      this.screen.x = opts.vanishPoint.x + x * this.screen.scale
+      this.screen.y = opts.vanishPoint.y + y * this.screen.scale
+    }
+
+    setConnection(connection: Connection) {
+      if (connection.isEnd) { this.reset() }
+
+      else {
+        this.connection = connection
+        this.nextConnection = connection.links[connection.links.length * Math.random() | 0]
+
+        this.ox = connection.x // original coordinates
+        this.oy = connection.y
+        this.oz = connection.z
+        this.os = connection.size // base size
+
+        this.nx = this.nextConnection.x // new
+        this.ny = this.nextConnection.y
+        this.nz = this.nextConnection.z
+        this.ns = this.nextConnection.size
+
+        this.dx = this.nx - this.ox // delta
+        this.dy = this.ny - this.oy
+        this.dz = this.nz - this.oz
+        this.ds = this.ns - this.os
+
+        this.proportion = 0
+      }
+    }
+
+    reset() {
+      this.setConnection(connections[0])
+      this.ended = 2
+    }
+
+    step() {
+      this.proportion += this.speed
+
+      if (this.proportion < 1) {
+        this.x = this.ox + this.dx * this.proportion
+        this.y = this.oy + this.dy * this.proportion
+        this.z = this.oz + this.dz * this.proportion
+        this.size = (this.os + this.ds * this.proportion) * opts.dataToConnectionSize
+      }
+      else { this.setConnection(this.nextConnection) }
+
+      this.screen.lastX = this.screen.x
+      this.screen.lastY = this.screen.y
+      this.setScreen()
+      this.screen.color = opts.dataColor
+        .replace('light', `${400 + ((tick * this.glowSpeed) % 500)}`)
+        .replace('alp', `${0.2 + (1 - this.screen.z / mostDistant) * 0.6}`)
+    }
+
+    draw() {
+      if (this.ended)
+        return --this.ended // not sre why the thing lasts 2 frames, but it does
+
+      ctx.beginPath()
+      ctx.strokeStyle = this.screen.color
+      ctx.lineWidth = this.size * this.screen.scale
+      ctx.moveTo(this.screen.lastX, this.screen.lastY)
+      ctx.lineTo(this.screen.x, this.screen.y)
+      ctx.stroke()
+    }
   }
-  Connection.prototype.setScreen = Data.prototype.setScreen = function () {
-    let x = this.x
-    let y = this.y
-    let z = this.z
-
-    // apply rotation on X axis
-    const Y = y
-    y = y * cosX - z * sinX
-    z = z * cosX + Y * sinX
-
-    // rot on Y
-    const Z = z
-    z = z * cosY - x * sinY
-    x = x * cosY + Z * sinY
-
-    this.screen.z = z
-
-    // translate on Z
-    z += opts.depth
-
-    this.screen.scale = opts.focalLength / z
-    this.screen.x = opts.vanishPoint.x + x * this.screen.scale
-    this.screen.y = opts.vanishPoint.y + y * this.screen.scale
-  }
-
-  function squareDist(a, b) {
+  interface Vector3 { x: number; y: number; z: number }
+  function squareDist(a: Vector3, b: Vector3) {
     const x = b.x - a.x
     const y = b.y - a.y
     const z = b.z - a.z
@@ -363,6 +496,7 @@ onMounted(() => {
     ctx.strokeStyle = opts.wireframeColor
     all.map((item) => {
       item.step()
+      return item
     })
     ctx.stroke()
     ctx.globalCompositeOperation = 'source-over'
@@ -371,6 +505,7 @@ onMounted(() => {
     })
     all.map((item) => {
       item.draw()
+      return item
     })
 
     /* ctx.beginPath();
@@ -516,21 +651,21 @@ function loginWrapper() {
           <div id="welcomeSubline3" class="welcomeSubline3" style="font-size:0.5vw;position: absolute; top: 4vw; height:0.4vw;width:0.4vw;">
             <div class="welcomeSublineBar3" style="margin-left:200%;position:absolute;top:0;left:0;height:100%; background:rgba(255,255,255,1)">
             </div>
-                        <div class="welcomeSublineTxt3" style="position:absolute; color:white; font-size:0.5vw;font-family:font5;width:4000%;margin:0;top:-40%;left:300%;opacity:0;">
+            <div class="welcomeSublineTxt3" style="position:absolute; color:white; font-size:0.5vw;font-family:font5;width:4000%;margin:0;top:-40%;left:300%;opacity:0;">
               Polling databse
             </div>
           </div>
           <div id="welcomeSubline4" class="welcomeSubline4" style="font-size:0.5vw;position: absolute; top: 4.5vw; height:0.4vw;width:0.4vw;">
             <div class="welcomeSublineBar4" style="margin-left:200%;position:absolute;top:0;left:0;height:100%; background:rgba(255,255,255,1)">
             </div>
-                        <div class="welcomeSublineTxt4" style="position:absolute; color:white; font-size:0.5vw;font-family:font5;width:4000%;margin:0;top:-40%;left:300%;opacity:0;">
+            <div class="welcomeSublineTxt4" style="position:absolute; color:white; font-size:0.5vw;font-family:font5;width:4000%;margin:0;top:-40%;left:300%;opacity:0;">
               Connecting through /dev/neuralTTY0
             </div>
           </div>
           <div id="welcomeSubline5" class="welcomeSubline5" style="font-size:0.5vw;position: absolute; top: 5vw; height:0.4vw;width:0.4vw;">
             <div class="welcomeSublineBar5" style="margin-left:200%;position:absolute;top:0;left:0;height:100%; background:rgba(255,255,255,1)">
             </div>
-                        <div class="welcomeSublineTxt5" style="position:absolute; color:white; font-size:0.5vw;font-family:font5;width:4000%;margin:0;top:-40%;left:300%;opacity:0;">
+            <div class="welcomeSublineTxt5" style="position:absolute; color:white; font-size:0.5vw;font-family:font5;width:4000%;margin:0;top:-40%;left:300%;opacity:0;">
               Welcome
             </div>
           </div>
