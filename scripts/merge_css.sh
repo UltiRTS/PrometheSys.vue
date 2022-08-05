9ce09fe6de5349e8dff465f71ce6dd6539d3339f
bianyuanop <m13320507359@163.com>
2022-08-05 10:45:43 +0800

    build css merge script

---
+++ scripts/merge_css.sh
@@ -0,0 +1,9 @@
+all_css=$(ls dist/renderer/*.css)
+index_css=$(ls dist/renderer/index*.css)
+
+for css in $all_css; do
+    echo $css
+    cat $css >> dist/renderer/all.css
+done
+
+mv dist/renderer/all.css $index_css
