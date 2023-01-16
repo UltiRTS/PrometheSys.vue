b3e3f9da002148ff9ffd169b37a1cc8d5a61ba2d
Alex Zhang <zhangchi0104@live.com>
2023-01-16 11:18:00 +0800

    [devtools] toolchain: added devtools

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,16 +1,20 @@
+import devtools from '@vue/devtools'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import pkg from '../../../package.json'
 import App from './App.vue'
 import router from './router'
-
 import './styles/index.scss'
 import 'uno.css'
 
 import './samples/node-api'
 const pinia = createPinia()
-
+if (process.env.NODE_ENV === 'development') {
+  console.log('Connecting to devtools')
+  devtools.connect('127.0.0.1', 8098)
+}
 const app = createApp(App)
-
 app.use(pinia)
   .use(router)
   .mount('#app')
