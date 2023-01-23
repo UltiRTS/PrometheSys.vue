b32b9da381c2b9693b4bd7d086b7ce0e18e4345f
Alex Zhang <zhangchi0104@live.com>
2023-01-23 10:20:09 +0800

    [devtool] chore: move vue-devtool into chrome-devtool

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -10,10 +10,7 @@ import 'uno.css'
 
 import './samples/node-api'
 const pinia = createPinia()
-if (process.env.NODE_ENV === 'development') {
-  console.log('Connecting to devtools')
-  devtools.connect('127.0.0.1', 8098)
-}
+
 const app = createApp(App)
 app.use(pinia)
   .use(router)
